ed4f5d3400c8c463b95fc3381ea8c8bb8d189a03
Leo <leolin310148@gmail.com>
2020-11-11 17:21:47 +0800

    1.0.40 add search with sorting

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew clean build bintrayUpload -Pversion=1.0.39
+./gradlew clean build bintrayUpload -Pversion=1.0.40
--- zygarde/src/main/kotlin/zygarde/data/jpa/dao/JpaSpecificationExecutorExtension.kt
+++ zygarde/src/main/kotlin/zygarde/data/jpa/dao/JpaSpecificationExecutorExtension.kt
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import zygarde.data.jpa.search.EnhancedSearch
 import zygarde.data.jpa.search.impl.EnhancedSearchImpl
 import zygarde.data.jpa.search.request.PagingAndSortingRequest
+import zygarde.data.jpa.search.request.SortingRequest
 import zygarde.data.jpa.search.request.asSort
 import zygarde.data.jpa.search.request.toPageRequest
 
@@ -25,6 +26,10 @@ private fun <T> buildSpec(searchContent: EnhancedSearch<T>.() -> Unit): Specific
 
 fun <T> JpaSpecificationExecutor<T>.search(searchContent: EnhancedSearch<T>.() -> Unit): List<T> {
   return findAll(buildSpec(searchContent))
+}
+
+fun <T> JpaSpecificationExecutor<T>.search(searchContent: EnhancedSearch<T>.() -> Unit, sorting: SortingRequest): List<T> {
+  return findAll(buildSpec(searchContent), sorting.asSort())
 }
 
 /**
