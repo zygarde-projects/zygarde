a6402d92273f05002770d2e3e9a7da9d5578a16f
carlos <echoulen@gmail.com>
2021-09-14 13:20:13 +0800

    fix typo

--- samples/v2-dsl-sample/sample-core/src/main/kotlin/sample/data/entity/Image.kt
+++ samples/v2-dsl-sample/sample-core/src/main/kotlin/sample/data/entity/Image.kt
@@ -1,13 +1,11 @@
 package sample.data.entity
 
-import zygarde.codegen.ZyModel
 import zygarde.codegen.meta.Comment
 import zygarde.codegen.meta.ZyModelMeta
 import zygarde.data.jpa.entity.AutoIntIdEntity
 import javax.persistence.Entity
 
 @ZyModelMeta
-@ZyModel
 @Entity
 open class Image(
   @Comment("image url")
