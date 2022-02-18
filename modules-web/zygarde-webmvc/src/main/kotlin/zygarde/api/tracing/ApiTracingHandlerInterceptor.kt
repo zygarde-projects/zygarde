a85ae0d9bdb51ec41cebe435aa97b223e516294d
Leo <leolin310148@gmail.com>
2022-02-18 17:36:19 +0800

    make ApiTracingHandlerInterceptor implement HandlerInterceptor by deprecation warning of HandlerInterceptorAdapter

--- modules-web/zygarde-webmvc/src/main/kotlin/zygarde/api/tracing/ApiTracingHandlerInterceptor.kt
+++ modules-web/zygarde-webmvc/src/main/kotlin/zygarde/api/tracing/ApiTracingHandlerInterceptor.kt
@@ -1,11 +1,11 @@
 package zygarde.api.tracing
 
 import org.springframework.web.method.HandlerMethod
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class ApiTracingHandlerInterceptor(val apiTracingContext: ApiTracingContext) : HandlerInterceptorAdapter() {
+class ApiTracingHandlerInterceptor(val apiTracingContext: ApiTracingContext) : HandlerInterceptor {
 
   override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
     if (handler is HandlerMethod) {
@@ -13,7 +13,6 @@ class ApiTracingHandlerInterceptor(val apiTracingContext: ApiTracingContext) : H
         "${handler.beanType.simpleName}.${handler.method.name}"
       )
     }
-
     return super.preHandle(request, response, handler)
   }
 }
