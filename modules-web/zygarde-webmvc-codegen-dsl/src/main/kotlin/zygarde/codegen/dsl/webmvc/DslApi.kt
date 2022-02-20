d29fe4b2d6f25e5615507df3a153e4957430795b
Leo <leolin310148@gmail.com>
2022-02-20 13:29:28 +0800

    add some inline Reified function for DslApi

--- modules-web/zygarde-webmvc-codegen-dsl/src/main/kotlin/zygarde/codegen/dsl/webmvc/DslApi.kt
+++ modules-web/zygarde-webmvc-codegen-dsl/src/main/kotlin/zygarde/codegen/dsl/webmvc/DslApi.kt
@@ -1,8 +1,11 @@
 package zygarde.codegen.dsl.webmvc
 
+import com.squareup.kotlinpoet.asTypeName
 import org.springframework.web.bind.annotation.RequestMethod
 import zygarde.codegen.model.ApiFunctionToGenerateVo
 import zygarde.codegen.model.ApiToGenerateVo
+import zygarde.data.api.PageDto
+import kotlin.reflect.KClass
 
 class DslApi(
   private val config: WebMvcDslCodegenConfig,
@@ -28,6 +31,60 @@ class DslApi(
     buildForMethod(functionName, path, RequestMethod.DELETE, dsl)
   }
 
+  @JvmName("getWithReified")
+  inline fun <reified REQ, reified RES> get(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.GET, REQ::class, emptyList(), RES::class, emptyList(), dsl)
+  }
+
+  @JvmName("postWithReified")
+  inline fun <reified REQ, reified RES> post(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.GET, REQ::class, emptyList(), RES::class, emptyList(), dsl)
+  }
+
+  @JvmName("putWithReified")
+  inline fun <reified REQ, reified RES> put(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.PUT, REQ::class, emptyList(), RES::class, emptyList(), dsl)
+  }
+
+  @JvmName("getWithReifiedForList")
+  inline fun <reified REQ, reified RES_IN_LIST> getList(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.GET, REQ::class, emptyList(), Collection::class, listOf(RES_IN_LIST::class), dsl)
+  }
+
+  @JvmName("postWithReifiedForList")
+  inline fun <reified REQ, reified RES_IN_LIST> postList(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.POST, REQ::class, emptyList(), Collection::class, listOf(RES_IN_LIST::class), dsl)
+  }
+
+  @JvmName("getWithReifiedForPage")
+  inline fun <reified REQ, reified RES_IN_LIST> getPage(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.GET, REQ::class, emptyList(), PageDto::class, listOf(RES_IN_LIST::class), dsl)
+  }
+
+  @JvmName("postWithReifiedForPage")
+  inline fun <reified REQ, reified RES_IN_LIST> postPage(functionName: String, path: String, crossinline dsl: (DslApiFunction.() -> Unit)) {
+    buildForMethodReified(functionName, path, RequestMethod.POST, REQ::class, emptyList(), PageDto::class, listOf(RES_IN_LIST::class), dsl)
+  }
+
+  inline fun buildForMethodReified(
+    functionName: String,
+    path: String,
+    method: RequestMethod,
+    reqClass: KClass<*>?,
+    reqClassGenericClasses: List<KClass<*>>,
+    resClass: KClass<*>?,
+    resClassGenericClasses: List<KClass<*>>,
+    crossinline dsl: DslApiFunction.() -> Unit
+  ) {
+    buildForMethod(functionName, path, method) {
+      requestType = reqClass?.takeUnless { it == Unit::class }?.asTypeName()
+      requestTypeGenericArguments = reqClassGenericClasses.map { it.asTypeName() }
+      responseType = resClass?.takeUnless { it == Unit::class }?.asTypeName()
+      responseTypeGenericArguments = resClassGenericClasses.map { it.asTypeName() }
+      dsl.invoke(this)
+    }
+  }
+
   fun toApiToGenerateVo(): ApiToGenerateVo {
     return ApiToGenerateVo(
       apiInterfacePackage = config.apiInterfacePackage,
@@ -40,7 +97,7 @@ class DslApi(
     )
   }
 
-  private fun buildForMethod(
+  fun buildForMethod(
     functionName: String,
     path: String,
     method: RequestMethod,
