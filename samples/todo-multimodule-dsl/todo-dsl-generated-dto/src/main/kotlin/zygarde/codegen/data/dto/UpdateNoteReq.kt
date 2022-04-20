8ce88f12ce7f8060b7cb6ea743037cd4fc3736ab
Leo <leolin310148@gmail.com>
2022-04-20 18:58:34 +0800

    add abstract model generated sample

---
+++ samples/todo-multimodule-dsl/todo-dsl-generated-dto/src/main/kotlin/zygarde/codegen/data/dto/UpdateNoteReq.kt
@@ -0,0 +1,14 @@
+package zygarde.codegen.`data`.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.io.Serializable
+import kotlin.String
+
+@Schema
+public data class UpdateNoteReq(
+  @Schema(
+    description="",
+    required=true
+  )
+  public val title: String
+) : Serializable
