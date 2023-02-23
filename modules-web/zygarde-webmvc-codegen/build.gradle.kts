d72f31694700e822a0f6f69d71a7a7cc7a134b8f
carlos <echoulen@gmail.com>
2023-02-23 13:30:41 +0800

    fix: imports and test jvm args

--- modules-web/zygarde-webmvc-codegen/build.gradle.kts
+++ modules-web/zygarde-webmvc-codegen/build.gradle.kts
@@ -30,3 +30,19 @@ dependencies {
 
 tasks.getByName("bootJar").enabled = false
 tasks.getByName("jar").enabled = true
+
+tasks.withType<Test> {
+  jvmArgs(
+    "--illegal-access=permit",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+  )
+}
