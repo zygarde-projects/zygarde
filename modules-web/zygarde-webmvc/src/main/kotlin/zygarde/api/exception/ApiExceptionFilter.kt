1d39276efae89f2257da175e6c9a4e63b75d2905
Leo <leolin310148@gmail.com>
2024-08-09 23:55:59 +0800

    feat: remove @Component on ApiExceptionFilter

--- modules-web/zygarde-webmvc/src/main/kotlin/zygarde/api/exception/ApiExceptionFilter.kt
+++ modules-web/zygarde-webmvc/src/main/kotlin/zygarde/api/exception/ApiExceptionFilter.kt
@@ -2,7 +2,6 @@ package zygarde.api.exception
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.GenericFilterBean
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
@@ -13,7 +12,6 @@ import javax.servlet.http.HttpServletResponse
 /**
  * @author leo
  */
-@Component
 open class ApiExceptionFilter(
   @Autowired val apiExceptionHandler: ApiExceptionHandler,
   @Autowired val objectMapper: ObjectMapper
