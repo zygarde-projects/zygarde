9972b7ffee478765882fdfbb40a7684c51c8872b
Leo <leolin310148@gmail.com>
2024-08-18 17:51:41 +0800

    feat: remove AuthExceptionFilter

--- modules-web/zygarde-webmvc-security/src/main/kotlin/zygarde/core/autoconfigure/ZygardeSpringSecurityConfig.kt
+++ modules-web/zygarde-webmvc-security/src/main/kotlin/zygarde/core/autoconfigure/ZygardeSpringSecurityConfig.kt
@@ -1,11 +1,8 @@
 package zygarde.core.autoconfigure
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import zygarde.api.exception.ApiExceptionHandler
-import zygarde.api.exception.filter.AuthExceptionFilter
 import zygarde.api.exception.mapper.AccessDeniedExceptionMapper
 
 @Configuration
@@ -14,11 +11,4 @@ class ZygardeSpringSecurityConfig {
   @ConditionalOnMissingBean
   @Bean
   fun accessDeniedExceptionMapper() = AccessDeniedExceptionMapper()
-
-  @ConditionalOnMissingBean
-  @Bean
-  fun authExceptionFilter(
-    apiExceptionHandler: ApiExceptionHandler,
-    objectMapper: ObjectMapper,
-  ) = AuthExceptionFilter(apiExceptionHandler, objectMapper)
 }
