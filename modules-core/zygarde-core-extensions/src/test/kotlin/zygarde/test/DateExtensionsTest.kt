cb8da7f766e44972b3b8316377b8b36121dd8467
Leo <leolin310148@gmail.com>
2025-12-03 14:59:46 +0800

    fix(test): make DateExtensionsTest timezone-independent

--- modules-core/zygarde-core-extensions/src/test/kotlin/zygarde/test/DateExtensionsTest.kt
+++ modules-core/zygarde-core-extensions/src/test/kotlin/zygarde/test/DateExtensionsTest.kt
@@ -6,15 +6,22 @@ import zygarde.core.extension.date.toDate
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.TimeZone
 
 class DateExtensionsTest {
+  private val zoneId = ZoneId.of("UTC")
+  private val timeZone = TimeZone.getTimeZone(zoneId)
+
   @Test
   fun `convert java8 LocalDate to java util date`() {
-    LocalDate.of(2000, 1, 1).toDate() shouldBe SimpleDateFormat("yyyy-MM-dd").parse("2000-01-01")
+    val sdf = SimpleDateFormat("yyyy-MM-dd").apply { this.timeZone = this@DateExtensionsTest.timeZone }
+    LocalDate.of(2000, 1, 1).toDate(zoneId) shouldBe sdf.parse("2000-01-01")
   }
 
   @Test
   fun `convert java8 LocalDateTime to java util date`() {
-    LocalDateTime.of(2000, 1, 1, 12, 0).toDate() shouldBe SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2000-01-01 12:00")
+    val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm").apply { this.timeZone = this@DateExtensionsTest.timeZone }
+    LocalDateTime.of(2000, 1, 1, 12, 0).toDate(zoneId) shouldBe sdf.parse("2000-01-01 12:00")
   }
 }
